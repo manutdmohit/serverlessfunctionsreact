a0603cb99e3da53a834946830cb51497f8b1c8ef
manutdmohit <saudmohit@gmail.com>
2021-09-27 10:27:08 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Airtable from './Airtable';
+import Basic from './Basic';
+
+const App = () => {
+  return (
+    <>
+      <Basic />
+      <Airtable />
+    </>
+  );
+};
+
+export default App;
